edf1c0597d44a44fa3bb9502f93a8b71c85bc330
SuperSuslik312 <SuperSuslik312@pm.me>
2023-04-30 20:45:57 +0300

    migrate from sqlite to postgresql

--- bot.py
+++ bot.py
@@ -1,14 +1,13 @@
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 from aiogram.dispatcher.handler import CancelHandler, current_handler
 
-from config import Config
 from ai import update, start_conversation
 from aiogram import Bot, Dispatcher, executor, types
 from aiogram.dispatcher.filters.state import StatesGroup, State
 from aiogram.dispatcher import FSMContext
 from aiogram.dispatcher.middlewares import BaseMiddleware
-from time import sleep
-from sqlite import *
+from asyncio import sleep
+from postgresql import *
 
 def set_key(key: str = None):
     def decorator(func):
@@ -23,7 +22,6 @@ bot = Bot(Config.BOT_TOKEN)
 storage = MemoryStorage()
 dp = Dispatcher(bot, storage=storage)
 previous_questions_and_answers = []
-instructions = Config.INSTRUCTIONS
 
 class AddUserToDB(BaseMiddleware):
     async def on_pre_process_message(self, message: types.Message, data: dict):
@@ -32,12 +30,12 @@ class AddUserToDB(BaseMiddleware):
 class Whitelist(BaseMiddleware):
     async def on_process_message(self, message: types.Message, data: dict):
         is_whitelisted = await read_whitelist(user_id=message.from_user.id)
-        if is_whitelisted != 1:
+        if is_whitelisted != True:
             await message.answer_chat_action('typing')
-            sleep(1.66)
+            await sleep(1.66)
             await message.answer("I'm under development, and no one can access me, except my creator.")
             await message.answer_chat_action('choose_sticker')
-            sleep(1.33)
+            await sleep(1.33)
             await message.answer_sticker("CAACAgIAAxkBAAEIvA1kSHmXeLZRAu03uPm1k8TZ54xTbAACWUAAAuCjggc35LUFXNY5gC8E")
             raise CancelHandler()
 
@@ -48,9 +46,9 @@ class Adminlist(BaseMiddleware):
             key = getattr(handler, 'key', None)
             if key == 'admin':
                 is_admin = await read_admin(user_id=message.from_user.id)
-                if is_admin != 1:
+                if is_admin != True:
                     await message.answer_chat_action('typing')
-                    sleep(1.66)
+                    await sleep(1.66)
                     await message.answer('У тебя нет доступа к этой команде!')
                     raise CancelHandler()
 
@@ -59,21 +57,22 @@ class Profile(StatesGroup):
     no_admin = State()
     is_whitelisted = State()
     no_whitelisted = State()
+    instructions = State()
 
 # Cancel any of command
 @dp.message_handler(commands='cancel', state='*')
-@set_key('admin')
 async def cancel(message: types.Message, state: FSMContext):
     if state is None:
         return
     await state.finish()
     await message.answer_chat_action('typing')
-    sleep(1.66)
+    await sleep(1.66)
     await message.answer('Ты прервал текущее действие!')
 
 # Start the conversation with bot
 @dp.message_handler(commands='start')
 async def start_bot(message: types.Message):
+    instructions = await read_instructions(user_id=message.from_user.id)
     await message.answer_chat_action('typing')
     response = start_conversation(instructions)
     await message.answer(response)
@@ -83,18 +82,17 @@ async def start_bot(message: types.Message):
 @set_key('admin')
 async def whitelistadd(message: types.Message):
     await message.answer_chat_action('typing')
-    sleep(1.66)
+    await sleep(1.66)
     await message.answer('Введи айди пользователя, которого хочешь добавить в вайтлист.')
     await Profile.is_whitelisted.set()
 
 @dp.message_handler(state=Profile.is_whitelisted)
 async def whitelistadd_finish(message: types.Message, state: FSMContext):
     async with state.proxy() as data:
-        data['user_id'] = message.text
-        data['is_whitelisted'] = 1
+        data['is_whitelisted'] = True
     await edit_whitelist(state, user_id=message.text)
     await message.answer_chat_action('typing')
-    sleep(1.66)
+    await sleep(1.66)
     await message.reply('Пользователь успешно добавлен в вайтлист!')
     await state.finish()
 
@@ -103,18 +101,17 @@ async def whitelistadd_finish(message: types.Message, state: FSMContext):
 @set_key('admin')
 async def whitelistdel(message: types.Message):
     await message.answer_chat_action('typing')
-    sleep(1.66)
+    await sleep(1.66)
     await message.answer('Введи айди пользователя, которого хочешь удалить из вайтлиста.')
     await Profile.no_whitelisted.set()
 
 @dp.message_handler(state=Profile.no_whitelisted)
 async def whitelistdel_finish(message: types.Message, state: FSMContext):
     async with state.proxy() as data:
-        data['user_id'] = message.text
-        data['is_whitelisted'] = 0
+        data['is_whitelisted'] = False
     await edit_whitelist(state, user_id=message.text)
     await message.answer_chat_action('typing')
-    sleep(1.66)
+    await sleep(1.66)
     await message.reply('Пользователь успешно удалён из вайтлиста!')
     await state.finish()
 
@@ -123,18 +120,17 @@ async def whitelistdel_finish(message: types.Message, state: FSMContext):
 @set_key('admin')
 async def adminadd(message: types.Message):
     await message.answer_chat_action('typing')
-    sleep(1.66)
+    await sleep(1.66)
     await message.answer('Введи айди пользователя, которого хочешь сделать админом.')
     await Profile.is_admin.set()
 
 @dp.message_handler(state=Profile.is_admin)
 async def adminadd_finish(message: types.Message, state: FSMContext):
     async with state.proxy() as data:
-        data['user_id'] = message.text
-        data['is_admin'] = 1
+        data['is_admin'] = True
     await edit_admin(state, user_id=message.text)
     await message.answer_chat_action('typing')
-    sleep(1.66)
+    await sleep(1.66)
     await message.reply('Пользователь успешно получил права админа!')
     await state.finish()
 
@@ -143,29 +139,58 @@ async def adminadd_finish(message: types.Message, state: FSMContext):
 @set_key('admin')
 async def admindel(message: types.Message):
     await message.answer_chat_action('typing')
-    sleep(1.66)
+    await sleep(1.66)
     await message.answer('Введи айди пользователя, у которого хочешь забрать права админа.')
     await Profile.no_admin.set()
 
 @dp.message_handler(state=Profile.no_admin)
 async def admindel_finish(message: types.Message, state: FSMContext):
     async with state.proxy() as data:
-        data['user_id'] = message.text
-        data['is_admin'] = 0
+        data['is_admin'] = False
     await edit_admin(state, user_id=message.text)
     await message.answer_chat_action('typing')
-    sleep(1.66)
+    await sleep(1.66)
     await message.reply('У пользователя успешно отобраны права админа!')
     await state.finish()
 
+# Set the system prompt for AI
+@dp.message_handler(commands='setprompt')
+async def setprompt(message: types.Message):
+    await message.answer_chat_action('typing')
+    await sleep(1.66)
+    await message.answer('Напиши в следующем сообщении что ты от меня хочешь!\n\nНапример если хочешь чтобы меня звали Куруми напиши "Тебя зовут Куруми" или "Твоё имя - Куруми" и т.д.\n\nМожешь писать несколько предложений, главное чтобы в одном сообщении!\n\nТакже ты можешь отменить это действие если нажмёшь /cancel')
+    await Profile.instructions.set()
+
+@dp.message_handler(state=Profile.instructions)
+async def setprompt_finish(message: types.Message, state: FSMContext):
+    async with state.proxy() as data:
+        data['instructions'] = message.text
+    await edit_instructions(state, user_id=message.from_user.id)
+    await message.answer_chat_action('typing')
+    await sleep(1.66)
+    await message.answer('Ты успешно задал мне поведение, спасибо!')
+    await message.answer_chat_action('choose_sticker')
+    await sleep(1.33)
+    await message.answer_sticker('CAACAgIAAxkBAAEIyFhkTaTnTzPFtQeYx4WaRUiYJglBnwACyj8AAuCjggcUTxrEwRdNXy8E')
+    await state.finish()
+
+@dp.message_handler(commands='whitelistget')
+@set_key('admin')
+async def whitelistget(message: types.Message):
+    user_ids = await get_whitelist()
+    await message.answer_chat_action('typing')
+    await sleep(1.66)
+    await message.answer(user_ids)
+
+# Clear the bot's memory
 @dp.message_handler(commands='clear')
 async def clear_history(message: types.Message):
     previous_questions_and_answers.clear()
     await message.answer_chat_action('typing')
-    sleep(1.66)
+    await sleep(1.66)
     await message.answer('Моя память успешно очищена, теперь я ничего не помню, ты доволен?')
     await message.answer_chat_action('choose_sticker')
-    sleep(1.33)
+    await sleep(1.33)
     await message.answer_sticker('CAACAgIAAxkBAAEIxMxkTDRAFYTvAUjeNeoc3PmmYba0tQAC7QsAAnkNSEhOUKpeKsKv6i8E')
 
 # Chatting with GPT-3.5
@@ -174,20 +199,21 @@ async def main(message: types.Message):
     try:
         await message.answer_chat_action('typing')
         new_question = message.text
+        instructions = await read_instructions(user_id=message.from_user.id)
         response = update(instructions, previous_questions_and_answers, new_question)
         previous_questions_and_answers.append((new_question, response))
         await message.answer(response)
     except Exception as e:
         print(e)
         await message.answer_chat_action('typing')
-        sleep(1.66)
+        await sleep(1.66)
         await message.answer("Прости пожалуйста! Во время обработки твоего запроса произошла какая-то ошибка.")
         await message.answer_chat_action('choose_sticker')
-        sleep(1.33)
+        await sleep(1.33)
         await message.answer_sticker("CAACAgIAAxkBAAEIvA9kSHs-b_bMbTJRFxkzFEFx8X5M5AACzz8AAuCjggd2g0I1aviuMS8E")
         await message.answer_chat_action('typing')
-        sleep(1.66)
-        await message.answer("Попробуй укоротить вопрос, очистить мне память, или, если ничего не поможет, свяжись с @SuperSuslik312")
+        await sleep(1.66)
+        await message.answer("Попробуй очистить мне память, или, если это не поможет, свяжись с @SuperSuslik312")
 
 # Bot polling
 if __name__ == '__main__':
