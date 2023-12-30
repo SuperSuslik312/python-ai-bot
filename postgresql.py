b1fcb81754f7f03b13603d7d3a158b8d247569a0
SuperSuslik312 <SuperSuslik312@pm.me>
2023-12-30 09:58:54 +0300

    better work with db (you need to recreate your db)

--- postgresql.py
+++ postgresql.py
@@ -12,68 +12,68 @@ instructions = Config.INSTRUCTIONS
 async def start_db():
     global db
     db = await asyncpg.connect(database=database, user=user, password=password, host=host, port=port)
-    await db.execute("CREATE TABLE IF NOT EXISTS profile(user_id INTEGER PRIMARY KEY, is_admin BOOL NOT NULL, is_whitelisted BOOL NOT NULL, instructions TEXT, history TEXT)")
+    await db.execute("CREATE TABLE IF NOT EXISTS profile(user_id INTEGER PRIMARY KEY, is_admin BOOL NOT NULL, is_whitelisted BOOL NOT NULL, instructions TEXT, history JSONB)")
 
 
 async def create(user_id):
-    user = await db.fetch(f"SELECT 1 FROM profile WHERE user_id = '{user_id}'")
+    user = await db.fetch("SELECT 1 FROM profile WHERE user_id = $1", user_id)
     if not user:
-        await db.execute(f"INSERT INTO profile VALUES('{user_id}', False, False, '{instructions}', '')")
+        await db.execute("INSERT INTO profile VALUES($1, False, False, $2, NULL)", user_id, instructions)
 
 
 async def edit_admin(state, user_id):
     await create(user_id)
     async with state.proxy() as data:
-        await db.execute(f"UPDATE profile SET is_admin = '{data['is_admin']}' WHERE user_id = '{user_id}'")
+        await db.execute(f"UPDATE profile SET is_admin = $1 WHERE user_id = $2", data['is_admin'], user_id)
 
 
 async def edit_whitelist(state, user_id):
     await create(user_id)
     async with state.proxy() as data:
-        await db.execute(f"UPDATE profile SET is_whitelisted = '{data['is_whitelisted']}' WHERE user_id = '{user_id}'")
+        await db.execute("UPDATE profile SET is_whitelisted = $1 WHERE user_id = $2", data['is_whitelisted'], user_id)
 
 
 async def edit_instructions(state, user_id):
     async with state.proxy() as data:
-        await db.execute(f"UPDATE profile SET instructions = '{data['instructions']}' WHERE user_id = '{user_id}'")
+        await db.execute("UPDATE profile SET instructions = $1 WHERE user_id = $2", data['isntructions'], user_id)
 
 
 async def reset_instructions(user_id):
-    await db.execute(f"UPDATE profile SET instructions = '{instructions}' WHERE user_id = '{user_id}'")
+    await db.execute("UPDATE profile SET instructions = $1 WHERE user_id = $2", instructions, user_id)
 
 
 async def edit_history(history, user_id):
-    await db.execute(f"UPDATE profile SET history = '{history}' WHERE user_id = '{user_id}'")
+    await db.execute("UPDATE profile SET history = $1 WHERE user_id = $2", history, user_id)
 
 
 async def clean_history(user_id):
-    await db.execute(f"UPDATE profile SET history = '' WHERE user_id = '{user_id}'")
+    await db.execute("UPDATE profile SET history = NULL WHERE user_id = $1", user_id)
 
 
 async def read_admin(user_id):
-    result = await db.fetchrow(f"SELECT is_admin FROM profile WHERE user_id = '{user_id}'")
+    result = await db.fetchrow("SELECT is_admin FROM profile WHERE user_id = $1", user_id)
     for res in result:
         return res
 
 
 async def read_whitelist(user_id):
-    result = await db.fetchrow(f"SELECT is_whitelisted FROM profile WHERE user_id = '{user_id}'")
+    result = await db.fetchrow("SELECT is_whitelisted FROM profile WHERE user_id = $1", user_id)
     for res in result:
         return res
 
 
 async def read_instructions(user_id):
-    result = await db.fetchrow(f"SELECT instructions FROM profile WHERE user_id = '{user_id}'")
+    result = await db.fetchrow("SELECT instructions FROM profile WHERE user_id = $1", user_id)
     for res in result:
         return res
 
 
 async def read_history(user_id):
-    result = await db.fetchrow(f"SELECT history FROM profile WHERE user_id = '{user_id}'")
+    result = await db.fetchrow("SELECT history FROM profile WHERE user_id = $1", user_id)
     for res in result:
         return res
 
 
 async def get_whitelist():
-    result = await db.fetch(f"SELECT user_id FROM profile WHERE is_whitelisted = True")
+    result = await db.fetch("SELECT user_id FROM profile WHERE is_whitelisted = True")
     return result
